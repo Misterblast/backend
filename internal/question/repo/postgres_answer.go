c1da5af10c94eb095a3e94b40021736b1ed7ebae
ghulammuzz <ghulamxiaomi@gmail.com>
2025-07-02 13:20:22 +0700

    fix: alphabet startwith in syncanswers API

--- internal/question/repo/postgres_answer.go
+++ internal/question/repo/postgres_answer.go
@@ -27,16 +27,18 @@ func (r *questionRepository) UpsertAndSyncAnswers(questionID int32, answers []qu
 
 	var insertValues []string
 	var insertArgs []interface{}
-
 	insertIdx := 1
 
 	if len(answers) == 0 {
 		return tx.Commit()
 	}
 
-	for _, ans := range answers {
+	for i, ans := range answers {
+		// Generate code: a, b, c, ...
+		generatedCode := string('a' + i)
+
 		insertValues = append(insertValues, fmt.Sprintf(`($%d, $%d, $%d, $%d, $%d)`, insertIdx, insertIdx+1, insertIdx+2, insertIdx+3, insertIdx+4))
-		insertArgs = append(insertArgs, questionID, ans.Code, ans.Content, ans.ImgURL, ans.IsAnswer)
+		insertArgs = append(insertArgs, questionID, generatedCode, ans.Content, ans.ImgURL, ans.IsAnswer)
 		insertIdx += 5
 	}
 
@@ -49,22 +51,5 @@ func (r *questionRepository) UpsertAndSyncAnswers(questionID int32, answers []qu
 		return app.NewAppError(500, "failed to insert new answers")
 	}
 
-	// if len(upsertValues) > 0 {
-	// 	upsertQuery := `
-	// 		INSERT INTO answers (id, question_id, code, content, img_url, is_answer) VALUES
-	// 	` + strings.Join(upsertValues, ", ") + `
-	// 		ON CONFLICT (id) DO UPDATE SET
-	// 			question_id = EXCLUDED.question_id,
-	// 			code = EXCLUDED.code,
-	// 			content = EXCLUDED.content,
-	// 			img_url = EXCLUDED.img_url,
-	// 			is_answer = EXCLUDED.is_answer
-	// 	`
-	// 	if _, err := tx.Exec(upsertQuery, upsertArgs...); err != nil {
-	// 		log.Error("[Repo][UpsertAndSyncAnswers] Error upserting answers: ", err)
-	// 		return app.NewAppError(500, "failed to upsert answers")
-	// 	}
-	// }
 	return tx.Commit()
-
 }
